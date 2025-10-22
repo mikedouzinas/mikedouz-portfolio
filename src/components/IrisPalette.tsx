8b67c7b7f7ad6d9f8fb6064e7c2154dbd92688f8
mikedouzinas <mikeveson@icloud.com>
2025-10-22 12:15:03 +0200

    fixed lint errors

--- src/components/IrisPalette.tsx
+++ src/components/IrisPalette.tsx
@@ -314,7 +314,7 @@ export default function IrisPalette({ open: controlledOpen, onOpenChange }: Iris
       setIsProcessingQuery(false);
       setSubmittedQuery('');
     }
-  }, [isAnimating, onOpenChange]);
+  }, [isAnimating, onOpenChange, isOpen]);
 
   /**
    * Fetch autocomplete suggestions from API
